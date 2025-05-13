933f31cff37fd2674ee2af07426fd6b5c79ae44c
niharika859 <niharika859@gmail.com>
2025-05-13 06:26:27 +0530

    Changed cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { styles } from '../styles';
 
 const Hero = () => {
   const handleDownloadResume = () => {
-    window.open('https://drive.google.com/file/d/1vxJZ4fnRzL9ticOKf31al7L8VSRz0gGU/view?usp=sharing', '_blank');
+    window.open('https://drive.google.com/file/d/1mnGxVoo-thycRfD9u9nppQEvXUEVbGGi/view?usp=sharing', '_blank');
   };
 
   return (
