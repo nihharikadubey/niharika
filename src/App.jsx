77cba76dd6fc1add5a6bc5359164de5195d9417d
nihharikadubey <niharika859@gmail.com>
2025-08-18 16:03:16 +0530

    fix: Remove scroll bar component - clean interface

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 // Import Hero and Navbar directly (not lazy loaded)
 import { Hero, Navbar } from './components';
-import ScrollBar from './components/ScrollBar';
 
 // Lazy load other components
 const About = lazy(() => import('./components/About'));
@@ -58,7 +57,6 @@ const HomePage = () => {
       {/* ALL PAGE CONTENT */}
       <div className='relative z-10'>
         <Navbar />
-        <ScrollBar />
         <Hero />
         
         {/* Other sections with Suspense for lazy loading */}
